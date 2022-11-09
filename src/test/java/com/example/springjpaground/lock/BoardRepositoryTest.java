7decef5675429e0e15d432771b75e0c9bd274d02
Hyunta <mohai2618@gmail.com>
2022-11-09 10:02:15 +0900

    test: 코드 포맷팅

--- src/test/java/com/example/springjpaground/lock/BoardRepositoryTest.java
+++ src/test/java/com/example/springjpaground/lock/BoardRepositoryTest.java
@@ -1,14 +1,11 @@
 package com.example.springjpaground.lock;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.Optional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -31,13 +28,13 @@ class BoardRepositoryTest {
     }
 
     @Test
-    void test() throws InterruptedException {
+    @DisplayName("낙관적 락이 잘 작동한다.")
+    void optimistic_lock() throws InterruptedException {
         Thread thread1 = new Thread(() -> boardService.update(1L));
         Thread thread2 = new Thread(() -> boardService.update(1L));
 
         thread1.start();
         thread2.start();
-
 
         thread1.join();
         thread2.join();
--- src/test/java/com/example/springjpaground/onetoone/application/ProductServiceTest.java
+++ src/test/java/com/example/springjpaground/onetoone/application/ProductServiceTest.java
@@ -6,6 +6,7 @@ import com.example.springjpaground.onetoone.domain.ProductPriceRepository;
 import com.example.springjpaground.onetoone.domain.ProductRepository;
 import java.util.List;
 import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,23 +24,28 @@ class ProductServiceTest {
 
 
     @Test
+    @DisplayName("연관관계의 주인을 조회하면 Lazy가 작동한다.")
     void oneToOne_Lazy() {
         Product product = new Product(null, new ProductPrice(null, null, 1000));
         productRepository.save(product);
 
         Product product1 = productRepository.findById(product.getId()).get();
+        List<Product> all = productRepository.findAll();
         System.out.println("product1 = " + product1);
+        System.out.println("all = " + all);
     }
 
     @Test
+    @DisplayName("연관관계의 주인이 아닌 쪽을 조회하면 Lazy가 작동하지 않는다.")
     void oneToOne_Lazy2() {
         ProductPrice productPrice = new ProductPrice(null, null, 1000);
         Product product = new Product(null, productPrice);
         productRepository.save(product);
 
         ProductPrice productPrice1 = productPriceRepository.findById(productPrice.getId()).get();
-        System.out.println("productPrice1 = " + productPrice1);
         List<ProductPrice> all = productPriceRepository.findAll();
+
+        System.out.println("productPrice1 = " + productPrice1);
         System.out.println("all = " + all);
     }
 }
