40f397a6f6b4aa0a13ac4506249267a06b71ef66
Hyunta <mohai2618@gmail.com>
2022-11-23 11:24:59 +0900

    feat: observable을 이용한 push 방식의 출력

---
+++ src/main/java/com/example/springjpaground/reactiveprogramming/ObservableExample.java
@@ -0,0 +1,38 @@
+package com.example.springjpaground.reactiveprogramming;
+
+import java.util.Observable;
+import java.util.Observer;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@SuppressWarnings("deprecation")
+public class ObservableExample {
+
+    static class IntObservable extends Observable implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 1; i <= 10; i++) {
+                setChanged();
+                notifyObservers(i); //push
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Observer ob = new Observer() {
+            @Override
+            public void update(Observable o, Object arg) {
+                System.out.println(Thread.currentThread().getName() + " = " + arg);
+            }
+        };
+
+        IntObservable io = new IntObservable();
+        io.addObserver(ob);
+
+        ExecutorService es = Executors.newSingleThreadExecutor();
+        es.execute(io);
+
+        System.out.println(Thread.currentThread().getName() + " = " + "EXIT");
+        es.shutdown();
+    }
+}
